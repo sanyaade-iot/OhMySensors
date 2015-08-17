fd402a2c1e4ecf61b8034deee94c4746d3163d6e
Marvin Roger <nivramdu94@gmail.com>
2015-08-17 18:30:04 +0200

    Implement controller functions, except req

--- migration/1439649425590-initial.js
+++ migration/1439649425590-initial.js
@@ -1,30 +1,32 @@
 var db = global.dbForMigration;
 
-var sql = "CREATE TABLE 'nodes' ("
-        + "  'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-        + "  'sketch_name' TEXT DEFAULT NULL,"
-        + "  'sketch_version' TEXT DEFAULT NULL,"
-        + "  'battery' INTEGER DEFAULT NULL"
-        + ");"
-        + ""
-        + "CREATE TABLE 'sensors' ("
-        + "  'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-        + "  'node_id' INTEGER NOT NULL REFERENCES 'nodes' ('id'),"
-        + "  'child_id' INTEGER NOT NULL,"
-        + "  'type' TEXT NOT NULL,"
-        + "  'description' TEXT NOT NULL"
-        + ");"
-        + ""
-        + "CREATE TABLE 'data' ("
-        + "  'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
-        + "  'sensor_id' INTEGER NOT NULL  REFERENCES 'sensors' ('id'),"
-        + "  'received_date' TEXT NOT NULL,"
-        + "  'type' TEXT NOT NULL,"
-        + "  'value' TEXT DEFAULT NULL"
-        + ");";
+var sqlNodes = "CREATE TABLE 'nodes' ("
+             + "  'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+             + "  'sketch_name' TEXT DEFAULT NULL,"
+             + "  'sketch_version' TEXT DEFAULT NULL,"
+             + "  'battery' INTEGER DEFAULT NULL"
+             + ");";
+
+var sqlSensors = "CREATE TABLE 'sensors' ("
+               + "  'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+               + "  'node_id' INTEGER NOT NULL REFERENCES 'nodes' ('id'),"
+               + "  'child_id' INTEGER NOT NULL,"
+               + "  'type' TEXT NOT NULL,"
+               + "  'description' TEXT NOT NULL"
+               + ");"
+
+var sqlData = "CREATE TABLE 'data' ("
+            + "  'id' INTEGER NOT NULL PRIMARY KEY AUTOINCREMENT,"
+            + "  'sensor_id' INTEGER NOT NULL REFERENCES 'sensors' ('id'),"
+            + "  'received_date' TEXT NOT NULL,"
+            + "  'type' TEXT NOT NULL,"
+            + "  'value' TEXT DEFAULT NULL"
+            + ");";
 
 exports.up = function(next) {
-  db.data.run(sql);
+  db.data.run(sqlNodes);
+  db.data.run(sqlSensors);
+  db.data.run(sqlData);
 
   db.config.set('configured', false, function() {
     next();
